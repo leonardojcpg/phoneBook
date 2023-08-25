4ac949ae6e46a152847bbf5db29b663cdd765c88
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-25 20:30:49 -0300

    feat: Creating templates and create.php

--- create.php
+++ create.php
@@ -2,8 +2,7 @@
     include_once("templates/header.php")
     
 ?>
-<h1>testando css</h1>
-<i class="fas fa-eye"></i>
+<h1>Add Contact</h1>
 
 <?php 
 
---
+++ templates/header.php
@@ -0,0 +1,39 @@
+<?php
+include_once("config/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- BOOTSRAP -->
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.1/css/bootstrap.rtl.min.css"
+        integrity="sha512-wO8UDakauoJxzvyadv1Fm/9x/9nsaNyoTmtsv7vt3/xGsug25X7fCUWEyBh1kop5fLjlcrK3GMVg8V+unYmrVA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+    <title>PhoneBook</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?= $BASE_URL ?>index.php">
+                <img src="<?= $BASE_URL ?>img/phoneBook.svg" alt="phoneBook">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a class="nav-link" id="home-link" href="<?= $BASE_URL ?>index.php">PhoneBook</a>
+                    <a class="nav-link" id="home-link" href="<?= $BASE_URL ?>create.php">Add Contact</a>
+                </div>
+            </div>
+        </nav>
+    </header>

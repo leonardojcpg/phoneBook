0857d45bf4d1a10cedd523d15f0415f6341130ed
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-25 23:43:31 -0300

    feat: Styling home page

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 include_once("templates/header.php");
 ?>
 
-<div>
+<div class="container">
     <?php if (isset($printMsg) && $printMsg != "") : ?>
     <p id="msg"><?= $printMsg ?></p>
     <?php endif; ?>
@@ -12,27 +12,26 @@ include_once("templates/header.php");
     <?php if (count($contacts) > 0) : ?>
     <table class="table" id="contacts-table">
         <thead>
-            <tr>
+            <tr class="primary-row">
                 <th scope="col">#</th>
                 <th scope="col">Name</th>
                 <th scope="col">Phone</th>
                 <th scope="col">Observations</th>
+                <th scope="col"></th>
             </tr>
         </thead>
         <tbody>
             <?php foreach($contacts as $contact): ?>
             <tr>
-                <td scope="row"><?= $contact['id'] ?></td>
+                <td scope="row" class="col-id"><?= $contact['id'] ?></td>
                 <td scope="row"><?= $contact['name'] ?></td>
                 <td scope="row"><?= $contact['phone'] ?></td>
                 <td scope="row"><?= $contact['observations'] ?></td>
 
-                <td>
-                    <a href="#">
-                        <i class="fas fa-eye check-icon"></i>
-                        <i class="fas fa-edit edit-icon"></i>
-                        <button type="submit" class="fas fa-times delete-icon"></button>
-                    </a>
+                <td class="actions">
+                    <a href="#"><i class="fas fa-eye check-icon"></i></a>
+                    <a href="#"><i class="fas fa-edit edit-icon"></i></a>
+                    <button type="submit" class="delete-button"><i class="fas fa-times delete-icon"></i></button>
                 </td>
             </tr>
             <?php endforeach; ?>

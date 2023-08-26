b4f27de5c1191aac7fb5a4329682cdc437097eb6
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-25 21:13:05 -0300

    feat: Creating database connection with Mysql PDO

---
+++ config/connection.php
@@ -0,0 +1,15 @@
+<?php 
+
+    $host = "localhost";
+    $database = "phonebook";
+    $user = "root";
+    $password = "";
+
+    try{
+        $connection = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+        $connection -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        
+    } catch (PDOException $e){
+        $error = $e->getMessage();
+        echo "Error: $error";
+    }

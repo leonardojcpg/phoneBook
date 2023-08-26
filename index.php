6d564725cc578718e6548f85d6bf827be9757de2
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-25 22:58:47 -0300

    feat: Creating Home page

--- index.php
+++ index.php
@@ -10,7 +10,34 @@ include_once("templates/header.php");
     <h1 id="main-title">My PhoneBook</h1>
 
     <?php if (count($contacts) > 0) : ?>
-    <p>Has Contacts</p>
+    <table class="table" id="contacts-table">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Phone</th>
+                <th scope="col">Observations</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($contacts as $contact): ?>
+            <tr>
+                <td scope="row"><?= $contact['id'] ?></td>
+                <td scope="row"><?= $contact['name'] ?></td>
+                <td scope="row"><?= $contact['phone'] ?></td>
+                <td scope="row"><?= $contact['observations'] ?></td>
+
+                <td>
+                    <a href="#">
+                        <i class="fas fa-eye check-icon"></i>
+                        <i class="fas fa-edit edit-icon"></i>
+                        <button type="submit" class="fas fa-times delete-icon"></button>
+                    </a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </tbody>
+    </table>
     <?php else : ?>
     <p id="empty-list-text">There's no contacts on your PhoneBook.
         <a href="<?= $BASE_URL ?>create.php">Click here to add a contact !</a>

9be76367774354cb87e501265eedff1cf7411aba
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-25 21:35:06 -0300

    feat: Creating the logic of theres no contact

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
-<?php 
-    include_once("templates/header.php")
-    
+<?php
+include_once("templates/header.php");
 ?>
-<h1>testando css</h1>
-<i class="fas fa-eye"></i>
 
-<?php 
+<div>
+    <?php if (isset($printMsg) && $printMsg != "") : ?>
+    <p id="msg"><?= $printMsg ?></p>
+    <?php endif; ?>
 
-    include_once("templates/footer.php")
+    <h1 id="main-title">My PhoneBook</h1>
+
+    <?php if (count($contacts) > 0) : ?>
+    <p>Has Contacts</p>
+    <?php else : ?>
+    <p id="empty-list-text">There's no contacts on your PhoneBook.
+        <a href="<?= $BASE_URL ?>create.php">Click here to add a contact !</a>
+    </p>
+    <?php endif; ?>
+</div>
+
+<?php
+include_once("templates/footer.php");
 ?>

5512ce683d24ed99cd93c633435785e40e41f3e9
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-26 21:38:51 -0300

    feat: Working on create contacts page

--- create.php
+++ create.php
@@ -1,10 +1,35 @@
-<?php 
-    include_once("templates/header.php")
-    
+<?php
+include_once("templates/header.php")
+
 ?>
-<h1>Add Contact</h1>
+<div class="container">
+    <?php include_once("templates/backButton.html") ?>
+    <h1 id="main-title">Add Contact</h1>
 
-<?php 
+    <form action="<?= $BASE_URL ?>config/process.php" method="POST">
+        <input type="hidden" name="type" value="create">
 
-    include_once("templates/footer.php")
+        <div class="form-group">
+            <label for="name">Name:</label>
+            <input type="text" class="form-control" id="name" name="name" placeholder="Type contact name" required>
+        </div>
+        <div class="form-group">
+            <label for="phone">Phone:</label>
+            <input type="text" class="form-control" id="phone" name="phone" placeholder="Type contact phone" required>
+        </div>
+        <div class="form-group">
+            <label for="observations">Observations:</label>
+            <textarea type="text" class="form-control" id="observations" name="observations" placeholder=" Type any
+                note" rows="3">
+            </textarea>
+        </div>
+
+        <button type="submit" class="btn btn-primary">Register</button>
+
+    </form>
+</div>
+
+<?php
+
+include_once("templates/footer.php")
 ?>

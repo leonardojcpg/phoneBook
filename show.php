67dfd8cf2c06ea9b3fa5082b7e5c43702ad55de4
leonardoGomes <leonardojpinheirogomes@gmail.com>
2023-08-26 21:17:26 -0300

    feat: Creating backButton component

--- show.php
+++ show.php
@@ -4,6 +4,7 @@
 ?>
 
 <div class="container" id="view-contact-container">
+    <?php include_once("templates/backButton.html") ?>
     <h1 id="main-title"><?= $contact['name'] ?></h1>
     <p class="bold"><b>Telefone:</b></p>
     <p><?= $contact['phone'] ?></p>
